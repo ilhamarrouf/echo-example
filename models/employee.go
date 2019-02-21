130ddaad6b9e59183d5df801d77d818b6685668d
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2019-02-21 12:05:20 +0700

    fixing data type on employee model

--- models/employee.go
+++ models/employee.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Employee struct {
-	Id     string `json:"id"`
+	Id     int    `json:"id"`
 	Name   string `json:"name"`
 	Salary string `json:"salary"`
 	Age    string `json:"age"`
